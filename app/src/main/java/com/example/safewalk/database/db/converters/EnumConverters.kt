f09ba97af5e41cf0f08aceeea2c88d2231067d03
BackSage <rafael.ferafa@gmail.com>
2026-01-28 13:39:23 -0300

    feat: adicionado enum type converts

---
+++ app/src/main/java/com/example/safewalk/database/db/converters/EnumConverters.kt
@@ -0,0 +1,28 @@
+package com.example.safewalk.database.db.converters
+
+import androidx.room.TypeConverter
+import com.example.safewalk.database.db.entity.enums.StatusAlerta
+import com.example.safewalk.database.db.entity.enums.TipoAlerta
+
+class EnumConverters {
+
+    @TypeConverter
+    fun fromTipoAlerta(tipo: TipoAlerta): String{
+        return tipo.name
+    }
+
+    @TypeConverter
+    fun toTipoAlerta(valor: String): TipoAlerta{
+        return TipoAlerta.valueOf(valor)
+    }
+
+    @TypeConverter
+    fun fromStatusAlerta(status: StatusAlerta): String{
+        return status.name
+    }
+
+    @TyperConverter
+    fun toStatusAlerta(valor: String): StatusAlerta{
+        return StatusAlerta.valueOf(valor)
+    }
+}
